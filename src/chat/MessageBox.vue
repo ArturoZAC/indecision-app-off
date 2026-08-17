d9a2ba06d6a5904901addde59f7fb1b418915403
ArturoZAC <arturoyz2105@gmail.com>
2026-08-16 19:40:37 -0500

    fix: update MessageBox to use keydown event and enhance message handling in IndecisionView

--- src/chat/MessageBox.vue
+++ src/chat/MessageBox.vue
@@ -5,7 +5,7 @@
       placeholder="Type your message..."
       class="flex-1 border rounded-full px-4 py-2 focus:outline-none"
       v-model="message"
-      @keypress.enter="sendMessage"
+      @keydown.enter="sendMessage"
     />
     <button
       class="bg-blue-500 text-white rounded-full p-2 ml-2 hover:bg-blue-600 focus:outline-none"
@@ -38,12 +38,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const emits = defineEmits<{
+  sendMessage: [text: string]
+}>()
+
 const message = ref<string>('')
 
 const sendMessage = () => {
   if (!message.value) return
 
-  console.log(message.value)
+  emits('sendMessage', message.value)
 
   message.value = ''
 }
--- src/views/IndecisionView.vue
+++ src/views/IndecisionView.vue
@@ -7,30 +7,35 @@
 
     <ChatMessages :messages="messages" />
 
-    <MessageBox />
-
-
+    <MessageBox @send-message="onMessage($event)" />
   </div>
 </template>
 
 <script setup lang="ts">
-  import ChatMessages from '@/chat/ChatMessages.vue'
-  import MessageBox from '@/chat/MessageBox.vue'
-  import type { ChatMessage } from '@/interfaces/chat-message.interface';
-  import { ref } from 'vue';
+import ChatMessages from '@/chat/ChatMessages.vue'
+import MessageBox from '@/chat/MessageBox.vue'
+import type { ChatMessage } from '@/interfaces/chat-message.interface'
+import { ref } from 'vue'
 
-  const messages = ref<ChatMessage[]>([
-    {
-      id: new Date().getTime(),
-      message: 'Hola Mundo',
-      isMine: true,
-    },
-    {
-      id: new Date().getTime() + 1,
-      message: 'Si',
-      isMine: false,
-      image: 'https://yesno.wtf/assets/no/8-5e08abbe5aacd2cf531948145b787e9a.gif',
-    },
-  ])
+const messages = ref<ChatMessage[]>([
+  {
+    id: new Date().getTime(),
+    message: 'Hola Mundo',
+    isMine: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'Si',
+    isMine: false,
+    image: 'https://yesno.wtf/assets/no/8-5e08abbe5aacd2cf531948145b787e9a.gif',
+  },
+])
 
+const onMessage = (text: string) => {
+  messages.value.push({
+    id: new Date().getTime(),
+    isMine: true,
+    message: text,
+  })
+}
 </script>
