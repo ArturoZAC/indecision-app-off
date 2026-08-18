e384b9e328c40893ef39be75cffcec0790699f60
ArturoZAC <arturoyz2105@gmail.com>
2026-08-18 15:33:36 -0500

    fix: add ref to chat container and adjust message handling in useChat

--- src/chat/ChatMessages.vue
+++ src/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <!-- Example Message -->
@@ -9,12 +9,30 @@
 </template>
 
 <script setup lang="ts">
-  import type { ChatMessage } from '@/interfaces/chat-message.interface.ts'
-  import ChatBubble from './ChatBubble.vue'
+  import type { ChatMessage } from '@/interfaces/chat-message.interface.ts';
+  import ChatBubble from './ChatBubble.vue';
+  import { nextTick, ref, watch } from 'vue';
 
   interface Props {
-    messages: ChatMessage[]
+    messages: ChatMessage[];
   }
 
-  defineProps<Props>()
+  const { messages } = defineProps<Props>();
+
+  const chatRef = ref<HTMLDivElement | null>();
+
+  watch(
+    () => messages,
+    () => {
+      nextTick(() => {
+        chatRef.value?.scrollTo({
+          top: chatRef.value.scrollHeight,
+          behavior: 'smooth',
+        });
+      });
+    },
+    {
+      deep: true,
+    },
+  );
 </script>
--- src/components/composables/useChat.ts
+++ src/components/composables/useChat.ts
@@ -1,4 +1,4 @@
-import { sleep } from '@/helpers/sleep';
+// import { sleep } from '@/helpers/sleep';
 import type { ChatMessage } from '@/interfaces/chat-message.interface';
 import type { YesNoResponse } from '@/interfaces/yes-no.response';
 import { ref } from 'vue';
@@ -24,7 +24,7 @@ export const useChat = () => {
 
     if (!text.endsWith('?')) return;
 
-    await sleep(1.5);
+    // await sleep(1.5);
 
     const { answer, image } = await getHerResponse();
 
